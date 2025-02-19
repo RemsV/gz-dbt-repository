17fc5a09bf6aaaaac5b61f31575e61dda1dbd62a
Rémi Vera <vera.remi22@gmail.com>
2025-02-19 15:37:15 +0000

    modification int campaigns avec fonction remplacement UNION ALL

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,15 +1,8 @@
-select
-*
-FROM {{ref('stg_raw__adwords')}}
-UNION ALL
-select
-*
-FROM {{ref('stg_raw__bing')}}
-UNION ALL
-select
-*
-FROM {{ref('stg_raw__criteo')}}
-UNION ALL
-select
-*
-FROM {{ref('stg_raw__facebook')}}
+{{ dbt_utils.union_relations(
+    relations=[
+        ref('stg_raw__adwords'),
+        ref('stg_raw__bing'),
+        ref('stg_raw__criteo'),
+        ref('stg_raw__facebook')
+    ]
+) }}
